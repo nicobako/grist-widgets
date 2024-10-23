b00e379b7bb3700b8f1e06b7abc26d98412fab16
Nico Bakomihalis <nico.bakomihalis@ierustech.com>
2024-10-23 09:42:31 -0500

    moving things around

---
+++ mermaid/app.js
@@ -0,0 +1,26 @@
+import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@11.3.0/+esm';
+
+async function initMermaid(){
+    mermaid.initialize({ startOnLoad: false });
+}
+
+async function runMermaid(graphDefinition) {
+    await initMermaid();
+    const { svg } = await mermaid.render('graphDiv', graphDefinition);
+    return svg;
+};
+
+async function initGrist(){
+    grist.ready({ requiredAccess: 'read table', columns: ["mermaid"] });
+    grist.onRecord(function (record) {
+        console.log("record", record);
+        const mapped = grist.mapColumnNames(record);
+        console.log("mapped", mapped);
+        const graphDefinition = mapped.mermaid;
+        runMermaid(graphDefinition).then((svg) => {
+            document.getElementById("mermaid-svg").innerHTML = svg;
+        })
+    });
+}
+
+document.addEventListener("DOMContentLoaded", initGrist)
