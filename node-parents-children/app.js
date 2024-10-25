2ca0e661477db2d83f95c7064014815651c5a1dd
Nico Bakomihalis <nico.bakomihalis@ierustech.com>
2024-10-25 15:26:29 -0500

    add node rendering

--- node-parents-children/app.js
+++ node-parents-children/app.js
@@ -2,9 +2,12 @@ function initGrist(){
     grist.ready({ requiredAccess: 'read table', columns: ["node", "children", "parents"] });
     grist.onRecord(function (record) {
         console.log("record", record);
-        const mapped = grist.mapColumnNames(record);
-        console.log("mapped variables", mapped);
     });
+}
+
+function renderPage(mapped){
+    const node = mapped.node;
+    document.getElementById("node").innerHTML = node;
 }
 
 document.addEventListener("DOMContentLoaded", initGrist);
