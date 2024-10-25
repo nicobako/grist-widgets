46a5a79f3e865053094031b800b2e3f86862c081
Nico Bakomihalis <nico.bakomihalis@ierustech.com>
2024-10-25 15:16:34 -0500

    forgot semi-colon

---
+++ node-parents-children/app.js
@@ -0,0 +1,10 @@
+function initGrist(){
+    grist.ready({ requiredAccess: 'read table', columns: ["node", "children", "parents"] });
+    grist.onRecord(function (record) {
+        console.log("record", record);
+        const mapped = grist.mapColumnNames(record);
+        console.log("mapped variables", mapped);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", initGrist);
