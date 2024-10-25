af7985606cbd09450fb0d0782ee1901f19f5bbf4
Nico Bakomihalis <nico.bakomihalis@ierustech.com>
2024-10-25 16:26:07 -0500

    linking nodes to table

--- node-parents-children/app.js
+++ node-parents-children/app.js
@@ -78,7 +78,7 @@ function renderNode(node){
     nodeElement.innerText = node;
     nodeElement.addEventListener("click", function(ev){
         console.log("node clicked", node, ev);
-        grist.setCursor({"Node": node});
+        grist.setCursorPos({"Node": node});
     });
     return nodeElement;
 }
