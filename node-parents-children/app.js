af248dd498051bb811f49b484201db5a91c060e7
Nico Bakomihalis <nico.bakomihalis@ierustech.com>
2024-10-25 15:47:21 -0500

    adding lists

--- node-parents-children/app.js
+++ node-parents-children/app.js
@@ -1,4 +1,4 @@
-function initGrist(){
+function initGrist() {
     grist.ready({ requiredAccess: 'read table', columns: ["node", "children", "parents"] });
     grist.onRecord(function (record) {
         console.log("record", record);
@@ -16,10 +16,33 @@ function initGrist(){
 
 }
 
-function renderPage(mapped){
+function renderPage(mapped) {
     const node = mapped.node;
     console.log("node", node);
-    document.getElementById("node").innerText = node;
+
+    const nodeElement = document.getElementById("node");
+    nodeElement.innerText = node;
+
+    const parentsElement = document.getElementById("parents");
+    const parents = mapped.parents;
+    renderList(parentsElement, parents);
+
+    const childrenElement = document.getElementById("children");
+    const children = mapped.children;
+    renderList(childrenElement, children);
+
+
 }
+
+function renderList(element, nodes){
+    clearList(element);
+    console.log(nodes);
+}
+
+function clearList(list) {
+    while (list.firstChild) {
+      list.removeChild(list.firstChild);
+    }
+  }
 
 document.addEventListener("DOMContentLoaded", initGrist);
