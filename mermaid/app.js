a12f7fc07c893d7fc0a9fe81fb7313dd6ea06940
Nico Bakomihalis <nico.bakomihalis@peopletec.com>
2025-11-18 12:37:39 -0600

    incremented mermaid version

--- mermaid/app.js
+++ mermaid/app.js
@@ -1,24 +1,24 @@
-import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@11.3.0/+esm';
+import mermaid from "https://cdn.jsdelivr.net/npm/mermaid@11.12.1/+esm";
 
-async function initMermaid(){
-    mermaid.initialize({ startOnLoad: false });
+async function initMermaid() {
+  mermaid.initialize({ startOnLoad: false });
 }
 
 async function runMermaid(graphDefinition) {
-    await initMermaid();
-    const { svg } = await mermaid.render('graphDiv', graphDefinition);
-    return svg;
-};
-
-async function initGrist(){
-    grist.ready({ requiredAccess: 'read table', columns: ["mermaid"] });
-    grist.onRecord(function (record) {
-        const mapped = grist.mapColumnNames(record);
-        const graphDefinition = mapped.mermaid;
-        runMermaid(graphDefinition).then((svg) => {
-            document.getElementById("mermaid-svg").innerHTML = svg;
-        })
-    });
+  await initMermaid();
+  const { svg } = await mermaid.render("graphDiv", graphDefinition);
+  return svg;
 }
 
-document.addEventListener("DOMContentLoaded", initGrist)
+async function initGrist() {
+  grist.ready({ requiredAccess: "read table", columns: ["mermaid"] });
+  grist.onRecord(function (record) {
+    const mapped = grist.mapColumnNames(record);
+    const graphDefinition = mapped.mermaid;
+    runMermaid(graphDefinition).then((svg) => {
+      document.getElementById("mermaid-svg").innerHTML = svg;
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", initGrist);
